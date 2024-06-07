08048868ebfea5d7b5be811372f5a3bd4f59284b
wpuerner <williampuerner@gmail.com>
2024-06-07 09:03:28 -0400

    add shuffling the deck and discard pile

--- deck_handler.gd
+++ deck_handler.gd
@@ -5,21 +5,30 @@ var rune_scene_paths: PackedStringArray = [
 	"res://scenes/runes/rapid_fire/rapid_fire.tscn",
 	"res://scenes/runes/explosive/explosive.tscn"
 ]
-var hand: Array
-var selected_runes: Array
+var hand: Array[Rune]
+var selected_runes: Array[Rune]
+var deck: Array[Rune]
+var discard: Array[Rune]
 
 func _ready():
 	_generate_deck()
 
 func _generate_deck():
 	for i in 10:
-		var rune_scene_path = rune_scene_paths[i % 2]
+		var rune_scene_path = rune_scene_paths[i % 3]
 		var rune = load(rune_scene_path).instantiate()
 		add_child(rune)
+		deck.append(rune)
+	_shuffle()
+		
+func _shuffle():
+	deck.append_array(discard)
+	discard.clear()
+	deck.shuffle()
 
 func get_hand() -> Array:
 	for i in 5:
-		hand.append(get_child(i))
+		hand.append(deck.pop_back())
 	return hand
 
 func activate_hand():
