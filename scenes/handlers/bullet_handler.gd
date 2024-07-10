f6d3e934c3e03a4b2c5fabde78ddf269936797e5
wpuerner <williampuerner@gmail.com>
2024-07-10 14:58:01 -0400

    enable pixel perfect rendering

--- scenes/handlers/bullet_handler.gd
+++ scenes/handlers/bullet_handler.gd
@@ -5,6 +5,7 @@ extends Node
 @export var num_bullets_attribute: Attribute
 @export var projectile_attribute: Attribute = preload("res://scenes/attributes/projectile/projectile_attribute.tres")
 
+@onready var spread_angle_attribute: Attribute = preload("res://scenes/attributes/spread_angle/spread_angle_attribute.tres")
 @onready var event_bus: Node = get_node("/root/EventBus")
 @onready var mana_handler: Node = get_node("/root/Main/ManaHandler")
 @onready var rng: RandomNumberGenerator = RandomNumberGenerator.new()
@@ -27,17 +28,21 @@ func _ready():
 func _physics_process(_delta):
 	if (timer.time_left > 0): return
 	if (automatic_fire_attribute.get_value() and Input.is_action_pressed("primary")) or (!automatic_fire_attribute.get_value() and Input.is_action_just_pressed("primary")):
-		for i in num_bullets_attribute.get_value():
-			_fire_bullet()
+		var direction = player.global_position.angle_to_point(player.get_global_mouse_position())
+		if num_bullets_attribute.get_value() == 1:
+			_fire_bullet(direction)
+		else:
+			for i in num_bullets_attribute.get_value():
+				_fire_bullet(direction + deg_to_rad(-0.5 * spread_angle_attribute.get_value() + (spread_angle_attribute.get_value() / (num_bullets_attribute.get_value() - 1)) * (i - 1)))
 		timer.start(60 / fire_rate_attribute.get_value())
 
-func _fire_bullet():
+func _fire_bullet(direction: float):
 	if !mana_handler.has_enough_mana(mana_cost): return
 	mana_handler.decrease_mana(mana_cost)
 	var bullet = projectile_attribute.get_value().instantiate()
 	bullet.global_position = player.global_position
 	add_child(bullet)
-	if bullet.has_method("init"): bullet.init()
+	if bullet.has_method("init"): bullet.init(direction)
 	
 func _reset():
 	mana_cost = 10
