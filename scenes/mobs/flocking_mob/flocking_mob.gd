6c965a79041a54dbe67a8e78ffa28029386a0bb1
wpuerner <williampuerner@gmail.com>
2025-01-29 10:14:38 -0500

    flocking mobs

--- scenes/mobs/flocking_mob/flocking_mob.gd
+++ scenes/mobs/flocking_mob/flocking_mob.gd
@@ -2,17 +2,19 @@ extends CharacterBody2D
 
 # Forward will be the positive x axis to make angle_to_point easier to process
 
-const STEERING_FORCE: float = 1.0
-const SEPARATION_FACTOR: float = 1.0
-const VIEW_ANGLE: float = 0.25 * PI  # The angle on either side of the forward axis where neighbors are visible
-const MOVE_SPEED: float = 20.0
+const STEERING_FORCE: float = 3.0
+const SEPARATION_FACTOR: float = 100.0
+const VIEW_ANGLE: float = 0.50 * PI  # The angle on either side of the forward axis where neighbors are visible
+const MOVE_SPEED: float = 80.0
 
 var target: Node2D
 var close_peers: Array[Node2D] = []
 
-func _physics_process(_delta):
+func _physics_process(delta: float):
 	if target == null:
 		return
+	
+	var steering_force: float = 0.0
 	
 	var visible_peers: Array[Node2D] = []
 	for peer in close_peers:
@@ -20,26 +22,28 @@ func _physics_process(_delta):
 		peer_position = peer_position.rotated(-global_rotation)
 		if abs(peer_position.angle()) < VIEW_ANGLE:
 			visible_peers.append(peer)
-		
-		# separation
+			
+			# correct for separation
+			if peer_position.length() <= velocity.length() * SEPARATION_FACTOR:
+				var m = 1 if peer_position.y < 0 else -1
+				steering_force += m * STEERING_FORCE * SEPARATION_FACTOR / peer_position.length()
 
-	var steering_force: float = 0
 	if !visible_peers.is_empty():
 		var average_peer_position: Vector2 = visible_peers \
 			.map(func(peer): return peer.global_position) \
 			.reduce(func(sum_position: Vector2, peer_position: Vector2): return sum_position + peer_position) \
 			 / visible_peers.size()
-		average_peer_position = average_peer_position.rotated(-global_rotation)
-		steering_force += -1 * STEERING_FORCE * average_peer_position.angle() / PI  # apply steering force towards the center of the visible neighbors
+		var relative_peer_position = average_peer_position - global_position
+		relative_peer_position = relative_peer_position.rotated(-global_rotation)
+		steering_force += STEERING_FORCE * relative_peer_position.angle() / PI
+	else:
+		var target_relative_position = target.global_position - global_position
+		target_relative_position = target_relative_position.rotated(-global_rotation)
+		steering_force += STEERING_FORCE * target_relative_position.angle() / PI
 
-	# alignment	
-	
-	# add steering force of target
-	var target_position = (target.global_position).rotated(-global_rotation)
-	steering_force += -1 * STEERING_FORCE * target_position.angle() / PI
-	
-	# move this body
-	velocity = Vector2(MOVE_SPEED, 0).rotated(global_rotation * steering_force)
+	# average the computed rotations for the global rotation
+	global_rotation += delta * steering_force
+	velocity = Vector2(MOVE_SPEED, 0).rotated(global_rotation)
 	move_and_slide()
 
 func _on_area_2d_body_entered(body: Node2D):
@@ -48,3 +52,9 @@ func _on_area_2d_body_entered(body: Node2D):
 
 func _on_area_2d_body_exited(body: Node2D):
 	close_peers.erase(body)
+
+func _get_correction_angle(from_angle: float, to_angle: float):
+	var direction: float = 1.0 if from_angle > to_angle else -1.0
+	while abs(from_angle - to_angle) > PI:
+		to_angle += direction * 2 * PI
+	return to_angle - from_angle
--- scenes/mobs/flocking_mob/flocking_mob_test.gd
+++ scenes/mobs/flocking_mob/flocking_mob_test.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
 func _ready():
+	var rng = RandomNumberGenerator.new()
+	
 	for child in get_children():
 		if child != $Target:
 			child.target = $Target
+			child.global_position = Vector2(randf_range(0, get_viewport_rect().size.x), randf_range(0, get_viewport_rect().size.y))
 
 func _physics_process(_delta):
 	$Target.global_position = get_global_mouse_position()
